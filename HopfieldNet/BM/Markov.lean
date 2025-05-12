b91c7cd03b13b8906af224997135f688121703cd
Matteo Cipollina <matteo.cipollina@yahoo.it>
2025-05-12 11:41:40 +0200

    Update Markov.lean

--- HopfieldNet/BM/Markov.lean
+++ HopfieldNet/BM/Markov.lean
@@ -3,7 +3,7 @@ Copyright (c) 2025 Matteo Cipollina. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Matteo Cipollina
 -/
-import HopfieldNet.BM 
+import HopfieldNet.BM.Core
 import HopfieldNet.Markov
 import Mathlib.Probability.Kernel.Basic
 import Mathlib.MeasureTheory.Measure.WithDensity
